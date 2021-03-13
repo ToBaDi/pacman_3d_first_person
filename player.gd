7718a6e05981e41feeea876f72d9e1630f595096
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-03-13 15:02:58 +0330

    fix performance issue by removing dots lights

--- player.gd
+++ player.gd
@@ -13,10 +13,6 @@ func _ready() -> void:
 	pass
 
 
-func _physics_process(_delta : float) -> void:
-	pass
-
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left", true):
 		rotate_left()
