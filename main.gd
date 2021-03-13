cc8ba2e445e959c6f785bd1998319ebd4685ffd6
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-03-13 15:51:08 +0330

    add Mirroring

--- main.gd
+++ main.gd
@@ -7,6 +7,10 @@ onready var ui : CanvasLayer = $UI
 onready var hud : CanvasLayer = $HUD
 
 
+func _ready() -> void:
+	print($Dots.get_child_count())
+
+
 func _exit_tree() -> void:
 	ui.queue_free()
 	hud.queue_free()
