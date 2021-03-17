683c827a63eb6212bc4c9f103d571ff795769ef7
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-03-17 15:42:54 +0330

    tweak Show and start setup font

--- show.gd
+++ show.gd
@@ -8,3 +8,14 @@ func show() -> void:
 
 func play() -> void:
 	$AnimationPlayer.play("Play")
+
+
+func night() -> void:
+	var pos : float = $AnimationPlayer.current_animation_position
+	$AnimationPlayer.stop(false)
+	$SkyAnimation.play("night")
+	while $SkyAnimation.is_playing():
+		yield(get_tree().create_timer(1), "timeout")
+	$AnimationPlayer.play("Play")
+	$AnimationPlayer.seek(pos, false)
+	pass
