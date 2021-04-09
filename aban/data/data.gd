658c695f3377cf1053d1416e96784e246dd21177
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-04-09 23:49:00 +0430

    player movement

--- aban/data/data.gd
+++ aban/data/data.gd
@@ -3,10 +3,12 @@ extends Node
 
 
 var is_on_show : bool = false
-var is_on_play_initiation : bool = false
+var is_on_play : bool = false
 
 onready var ground : MeshInstance = $"../Scene/Map/Ground"
-onready var player_camera : Camera = $"../Scene/PlayerCamera"
+onready var player : KinematicBody = $"../Scene/Player"
+onready var player_camera : Camera = $"../Scene/Player/Camera"
+onready var player_raycast : RayCast = $"../Scene/Player/RayCast"
 onready var show_node : Spatial = $"../Scene/Show"
 onready var show_camera : Camera = $"../Scene/Show/ShowCamera"
 onready var animation_player : AnimationPlayer = $"../Scene/AnimationPlayer"
