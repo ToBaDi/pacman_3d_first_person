d6e1387e3f7753b982cbc507c1a93f590317c904
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-04-09 22:36:05 +0430

    contiuing

--- aban/data/data.gd
+++ aban/data/data.gd
@@ -2,12 +2,15 @@ class_name Data
 extends Node
 
 
+signal play_initiation_finished
+
+
 var space_state : PhysicsDirectSpaceState
 var is_on_show : bool = false
 var is_on_play_initiation : bool = false
 
 onready var ground : MeshInstance = $"../Scene/Map/Ground"
-onready var playerCamera : Camera = $"../Scene/PlayerCamera"
+onready var player_camera : Camera = $"../Scene/PlayerCamera"
 onready var show_node : Spatial = $"../Scene/Show"
 onready var show_camera : Camera = $"../Scene/Show/ShowCamera"
 onready var animation_player : AnimationPlayer = $"../Scene/AnimationPlayer"
